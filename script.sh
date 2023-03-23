42abd67a873a98d6f90792abfab552db127e2921
OutlookAcconunt0102930492819 <128750151+OutlookAcconunt0102930492819@users.noreply.github.com>
2023-03-23 18:11:11 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,1 @@
-echo "Oi, isto é um teste de loop"
-./script.sh
+sudo apt update && curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok && ngrok config add-authtoken 2NQob9ICFq1whSLYlvfigWiVpcT_2GRfuGGZJjohoAZGxkHP1 && 
